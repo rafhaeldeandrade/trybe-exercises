c5d3b4bb45002f88ef3691055cf46bf4d5ce9d92
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-06-25 19:12:50 -0300

    [6.2] css & js frameworks

---
+++ 1 - web development fundamentals/section_06_forms_flexbox_responsive/day_2/exercises/js/script.js
@@ -0,0 +1,57 @@
+const btnSubmit = document.getElementById("btn-submit");
+const btnReset = document.getElementById("btn-reset");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputCpf = document.getElementById("cpf");
+const inputAddress = document.getElementById("address");
+const inputCity = document.getElementById("city");
+const inputState = document.getElementById("state");
+const houseType = document.getElementById("house");
+const apartmentType = document.getElementById("apartment");
+const dataInicio = document.getElementById("data-inicio");
+
+const states = [
+  "Acre",
+  "Alagoas",
+  "Amapá",
+  "Amazonas",
+  "Bahia",
+  "Ceará",
+  "Espírito Santo",
+  "Goías",
+  "Maranhão",
+  "Mato Grosso",
+  "Mato Grosso do Sul",
+  "Minas Gerais",
+  "Pará",
+  "Paraíba",
+  "Paraná",
+  "Pernambuco",
+  "Piauí",
+  "Rio de Janeiro",
+  "Rio Grande do Norte",
+  "Rio Grande do Sul",
+  "Rondônia",
+  "Roraima",
+  "Santa Catarina",
+  "São Paulo",
+  "Sergipe",
+  "Tocantins",
+  "Distrito Federal",
+];
+
+function insereEstados() {
+  for (let estado of states) {
+    let option = document.createElement("option");
+    option.setAttribute("value", estado);
+    option.innerText = estado;
+    inputState.appendChild(option);
+  }
+}
+
+insereEstados();
+
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
