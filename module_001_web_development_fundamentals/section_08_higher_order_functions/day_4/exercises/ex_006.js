21240bdffc7be8eb22b2741b0a916cac541cb6e7
Rafhael Gomes <rafhael.andrade@hotmail.com.br>
2021-07-07 23:57:47 -0300

    [9.2] JavaScript promises exercises

--- module_001_web_development_fundamentals/section_08_higher_order_functions/day_4/exercises/ex_006.js
+++ module_001_web_development_fundamentals/section_08_higher_order_functions/day_4/exercises/ex_006.js
@@ -7,24 +7,17 @@ function studentAverage() {
   // escreva seu código aqui
   const media = grades.reduce((acc, curr) => {
     acc.push(curr.reduce((notaAcc, notaCurr, index, array) => {
-      if (index === array.length -1) {
-        return (notaAcc + notaCurr) / array.length
+      if (index === array.length - 1) {
+        return (notaAcc + notaCurr) / array.length;
       }
-      return notaAcc += notaCurr;
+      return { (notaAcc += notaCurr) };
     }));
     return acc;
   }, []);
   
-  const returnArray = students.map((nome, index) => {
-    return { name: nome, average: media[index] }
-  })
+  const returnArray = students.map((nome, index) => ({ name: nome, average: media[index] }));
   return returnArray;
 }
 
-const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-];
 
 assert.deepStrictEqual(studentAverage(), expected);
